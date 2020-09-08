de719d7cf7fe70541ac12cba933afed352b21ee6
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-09-08 10:40:42 -0300

    [Setup] Basic server and client classes template

--- client/src/main/java/ar/edu/itba/pod/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/client/Client.java
@@ -3,10 +3,21 @@ package ar.edu.itba.pod.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Client {
-    private static Logger logger = LoggerFactory.getLogger(Client.class);
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-    public static void main(String[] args) {
-        logger.info("POD-TPE1 Client Starting ...");
+public class Client {
+    private static final Logger LOG = LoggerFactory.getLogger(Client.class);
+
+    public static void main(final String[] args) throws RemoteException, NotBoundException, MalformedURLException, InterruptedException {
+        LOG.info("Client has started...");
+        //final GenericService service = (GenericService) Naming.lookup("//127.0.0.1:1099/service");
+
+        //UserAvailableCallbackHandler handler = new UserAvailableCallbackHandlerImpl();
+
+        //UnicastRemoteObject.exportObject(handler, 0);
     }
 }
--- server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -3,10 +3,23 @@ package ar.edu.itba.pod.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Server {
-    private static Logger logger = LoggerFactory.getLogger(Server.class);
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
-    public static void main(String[] args) {
-        logger.info("POD-TPE1 Server Starting ...");
+public class Server {
+    private static final Logger LOG = LoggerFactory.getLogger(Server.class);
+
+    public static void main(final String[] args) throws RemoteException, AlreadyBoundException {
+        LOG.info("Server Starting ...");
+        //final GenericService gs = new GenericServiceImpl();
+        //final Remote remote = UnicastRemoteObject.exportObject(gs, 0);
+
+        final Registry registry = LocateRegistry.getRegistry();
+        //registry.bind("service", remote);
+        LOG.info("Service bound");
     }
 }
