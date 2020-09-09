75eb6eff449543714990d7e9bd091d5e2b97d795
Marina <mfuster@itba.edu.ar>
2020-09-09 15:17:25 -0300

    Vote updated to include more information

--- api/src/main/java/ar/edu/itba/pod/Vote.java
+++ api/src/main/java/ar/edu/itba/pod/Vote.java
@@ -1,14 +1,24 @@
 package ar.edu.itba.pod;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 public class Vote implements Serializable {
-    private Party party;
-    private Integer table; // FIXME puede ser int -> como prefieran
+    private final Party party;
+    private final Province province;
+    private final Integer table; // FIXME puede ser int -> como prefieran
+    private final Party fptpVote;
+    private final Map<Party, Long> starVote;
+    private final List<Party> spavVote;
 
-    public Vote(Party party, Integer table) {
+    public Vote(Party party, Province province, Integer table, Party fptpVote, Map<Party, Long> starVote, List<Party> spavVote) {
         this.party = party;
         this.table = table;
+        this.province = province;
+        this.fptpVote = fptpVote;
+        this.starVote = starVote;
+        this.spavVote = spavVote;
     }
 
     public Party getParty() {
@@ -17,5 +27,21 @@ public class Vote implements Serializable {
 
     public Integer getTable() {
         return table;
+    }
+
+    public Province getProvince() {
+        return province;
+    }
+
+    public Party getFptpVote() {
+        return fptpVote;
+    }
+
+    public Map<Party, Long> getStarVote() {
+        return starVote;
+    }
+
+    public List<Party> getSpavVote() {
+        return spavVote;
     }
 }
--- server/src/main/java/ar/edu/itba/pod/server/models/NationalElection.java
+++
@@ -1,4 +0,0 @@
-package ar.edu.itba.pod.server.models;
-
-public class NationalElection {
-}
