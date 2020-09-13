a10d652769749200fee320042cbd78d6c583cc1d
Gaston Lifschitz <glifschitz@itba.edu.ar>
2020-09-13 15:23:42 -0300

    Add state query

--- client/src/main/java/ar/edu/itba/pod/client/QueryClient.java
+++ client/src/main/java/ar/edu/itba/pod/client/QueryClient.java
@@ -4,10 +4,7 @@ import ar.edu.itba.pod.*;
 import ar.edu.itba.pod.client.arguments.QueryClientArguments;
 import ar.edu.itba.pod.client.exceptions.InvalidArgumentsException;
 import ar.edu.itba.pod.exceptions.InvalidElectionStateException;
-import ar.edu.itba.pod.models.Party;
-import ar.edu.itba.pod.models.Province;
-import ar.edu.itba.pod.models.ElectionResults;
-import ar.edu.itba.pod.models.VotingType;
+import ar.edu.itba.pod.models.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +14,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
@@ -59,10 +57,38 @@ public class QueryClient {
         }else{
             try {
                 ElectionResults stateResults = service.getProvinceResults(Province.fromValue(clientArguments.getProvinceName()));
+                stateQuery(stateResults,clientArguments);
             } catch (InvalidElectionStateException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void stateQuery(ElectionResults stateResults, QueryClientArguments clientArguments) {
+        StateElectionsResult stateElectionsResult = stateResults.getStateElectionsResult();
+        TreeSet<Map.Entry<Party,Double>> firstRound = stateElectionsResult.getFirstRound();
+        TreeSet<Map.Entry<Party,Double>> secondRound = stateElectionsResult.getSecondRound();
+        TreeSet<Map.Entry<Party,Double>> thirdRound = stateElectionsResult.getThirdRound();
+        List<Party> winners = stateElectionsResult.getWinners();
+
+        String outputString = "Round 1\nApproval;Party" +
+                getStringFromTreeSet(firstRound) +
+                "\nWinners\n" + winners.stream().findFirst() + "\nRound 2\nApproval;Party" +
+                getStringFromTreeSet(secondRound) +
+                "\nWinners\n" + winners.stream().findFirst() + ", " + winners.get(1) + "\nRound 3\nApproval;Party" +
+                getStringFromTreeSet(thirdRound) +
+                "\nWinners\n" + winners.stream().findFirst() + ", " + winners.get(1) + ", " + winners.get(2);
+        write(clientArguments.getOutPutPath(), outputString);
+    }
+    private static String getStringFromTreeSet(TreeSet<Map.Entry<Party,Double>> round){
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<Party, Double> pair : round){
+            if(pair.getValue()!=null){
+                sb.append("\n");
+                sb.append(pair.getValue()).append(";").append(pair.getKey());
+            }
+        }
+        return sb.toString();
     }
 
     private static void ftptQuery(ElectionResults results, QueryClientArguments clientArguments, boolean showWinner) {
